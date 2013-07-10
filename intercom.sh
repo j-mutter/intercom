20e7450e4c3af656505eb554ed213472e65347e4
Tom Burns <tom.burns@jadedpixel.com>
2013-07-10 17:03:27 -0400

    explicit paths

--- intercom.sh
+++ intercom.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 while [ 1 ]; do
-  redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message > /tmp/intercom-new;
+  /opt/boxen/homebrew/bin/redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message > /tmp/intercom-new;
   NEW=`md5 -q /tmp/intercom-new 2>/dev/null`
   OLD=`md5 -q /tmp/intercom-last 2>/dev/null`
   if [ "$NEW" != "$OLD" ]; then
-    cat /tmp/intercom-new | say
+    cat /tmp/intercom-new | /usr/bin/say
     mv /tmp/intercom-new /tmp/intercom-last
   fi
   sleep 10
