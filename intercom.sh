329006c0220c10133d3fe1671da2a623de251d7f
Oren Mazor <oren.mazor@gmail.com>
2013-07-10 17:18:21 -0400

    autosave to itunes

--- intercom.sh
+++ intercom.sh
@@ -6,6 +6,10 @@ while [ 1 ]; do
   OLD=`md5 -q /tmp/intercom-last 2>/dev/null`
   if [ "$NEW" != "$OLD" ]; then
     cat /tmp/intercom-new | /usr/bin/say
+    if [ "$1" == "save" ]; then
+      cat /tmp/intercom-new | /usr/bin/say -o /tmp/intercom.m4a
+      osascript -e "tell application \"iTunes\" to add POSIX file \"/tmp/intercom.m4a\""
+    fi
     mv /tmp/intercom-new /tmp/intercom-last
   fi
   sleep 10
