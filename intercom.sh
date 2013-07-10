31198f97daadfd1e9b19c1b105755d496228496f
Tom Burns <tom.burns@jadedpixel.com>
2013-07-10 18:54:38 -0400

    use diff instead of md5

--- intercom.sh
+++ intercom.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 while [ 1 ]; do
   redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message > /tmp/intercom-new;
-  NEW=`md5 -q /tmp/intercom-new 2>/dev/null`
-  OLD=`md5 -q /tmp/intercom-last 2>/dev/null`
-  if [ "$NEW" != "$OLD" ]; then
+  DIFF=`diff -q /tmp/intercom-new /tmp/intercom-last 2>/dev/null`
+  if [ "$DIFF" != "" ]; then
     cat /tmp/intercom-new | say
     mv /tmp/intercom-new /tmp/intercom-last
   fi
