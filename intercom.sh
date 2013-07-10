812e3228b88c43cf74ed5121b29b42fe874a0de1
Tom Burns <tom.burns@jadedpixel.com>
2013-07-10 16:27:53 -0400

    MVP lean startup compressed voip (tm) (tm) (tm)

---
+++ intercom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [ 1 ]; do
+  redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message > /tmp/intercom-new;
+  NEW=`md5 -q /tmp/intercom-new 2>/dev/null`
+  OLD=`md5 -q /tmp/intercom-last 2>/dev/null`
+  if [ "$NEW" != "$OLD" ]; then
+    cat /tmp/intercom-new | say
+    mv /tmp/intercom-new /tmp/intercom-last
+  fi
+  sleep 10
+done
