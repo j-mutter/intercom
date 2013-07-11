3006f1e12128b8cde3d4bf3fb08c1f5f9bb6a51f
Oren Mazor <oren.mazor@gmail.com>
2013-07-11 11:55:47 -0400

    fix save to iTunes

--- intercom.sh
+++ intercom.sh
@@ -7,7 +7,8 @@ while [ 1 ]; do
     echo $MESSAGE >> /tmp/intercom.log
     say $MESSAGE
     if [ "$1" == "save" ]; then
-      osascript -e "tell application \"iTunes\" to add POSIX file \"/tmp/intercom.m4a\""
+      say $MESSAGE -o /tmp/intercom.m4a
+      osascript -e "tell application \"iTunes\"" -e "set newFile to POSIX file \"/tmp/intercom.m4a\"" -e "add newFile to playlist \"shopify intercoms\"" -e "set newTrack to (add newFile)" -e "set name of newTrack to \"$MESSAGE\"" -e "end tell"
     fi
     LAST=$MESSAGE
   fi
