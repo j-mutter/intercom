a65080bb9634349197080432c2eea11368d2a24d
Oren Mazor <oren.mazor@gmail.com>
2013-07-11 11:19:21 -0400

    log to background

--- intercom.sh
+++ intercom.sh
@@ -3,8 +3,8 @@ LAST=0
 
 while [ 1 ]; do
   MESSAGE=`redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message`;
-  if [ $MESSAGE != $LAST ]; then
-    echo $MESSAGE
+  if [ "$MESSAGE" != "$LAST" ]; then
+    echo $MESSAGE >> /tmp/intercom.log
     say $MESSAGE
     if [ "$1" == "save" ]; then
       osascript -e "tell application \"iTunes\" to add POSIX file \"/tmp/intercom.m4a\""
