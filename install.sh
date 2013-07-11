ae47b56513593d72906438e8a87e7d55857b0589
Oren Mazor <oren.mazor@gmail.com>
2013-07-11 11:02:21 -0400

    set environment variable for intercom

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ curl -o $INTERCOM_BIN https://raw.github.com/boourns/intercom/master/intercom.sh
 chmod +x $INTERCOM_BIN
 echo "I am going to ask for SUDO to install intercom as a service. Dont panic!"
 sudo launchctl remove com.boourns.intercom
+sudo launchctl setenv INTERCOM_REDIS_CONFIG "$INTERCOM_REDIS_CONFIG"
 sudo cp ./com.boourns.intercom.plist /Library/LaunchDaemons/com.boourns.intercom.plist
 sudo launchctl load -w /Library/LaunchDaemons/com.boourns.intercom.plist
 echo "done!"
