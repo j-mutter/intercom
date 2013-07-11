c6331761ba1c3d7c328893630183517653bf27f6
Justin Mutter <mutter.justin@gmail.com>
2013-07-11 11:44:33 -0400

    Because say isn't enough, post to notification center

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ PREFIX="/usr/local/bin"
 INTERCOM_BIN="$PREFIX/intercom"
 curl -o $INTERCOM_BIN https://raw.github.com/boourns/intercom/master/intercom.sh
 chmod +x $INTERCOM_BIN
+
+gem install terminal-notifier
--- intercom.sh
+++ intercom.sh
@@ -6,7 +6,9 @@ while [ 1 ]; do
   redis-cli $INTERCOM_REDIS_CONFIG GET intercom-message > /tmp/intercom-new;
   DIFF=`diff -q /tmp/intercom-new /tmp/intercom-last 2>/dev/null`
   if [ "$DIFF" != "" ]; then
-    cat /tmp/intercom-new | say
+    MESSAGE=`cat /tmp/intercom-new`
+    say $MESSAGE
+    terminal-notifier  -title "Intercom" -message "$MESSAGE"
     if [ "$1" == "save" ]; then
       cat /tmp/intercom-new | /usr/bin/say -o /tmp/intercom.m4a
       osascript -e "tell application \"iTunes\" to add POSIX file \"/tmp/intercom.m4a\""
