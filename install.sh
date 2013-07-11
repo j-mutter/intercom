e42085653f5df7e52dc851b12060f8eb44375a69
Oren Mazor <oren.mazor@gmail.com>
2013-07-11 10:25:56 -0400

    add launchctl support

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ PREFIX="/usr/local/bin"
 INTERCOM_BIN="$PREFIX/intercom"
 curl -o $INTERCOM_BIN https://raw.github.com/boourns/intercom/master/intercom.sh
 chmod +x $INTERCOM_BIN
+echo "I am going to ask for SUDO to install intercom as a service. Dont panic!"
+sudo launchctl remove com.boourns.intercom
+sudo cp ./com.boourns.intercom.plist /Library/LaunchDaemons/com.boourns.intercom.plist
+sudo launchctl load -w /Library/LaunchDaemons/com.boourns.intercom.plist
+echo "done!"
