9ca6a23d675ce544a881f18f9d388e16024279b6
Tom Burns <tom.burns@jadedpixel.com>
2013-07-11 16:01:46 -0400

    fix remote code execution

--- intercom.rb
+++ intercom.rb
@@ -4,13 +4,30 @@ def access_url
   ENV['INTERCOM_URL']
 end
 
+def set_message(message)
+  if message.match(/\A[\w\s-]*\z/)
+    $message = message
+  else
+    "....................../´¯/) 
+....................,/¯../ 
+.................../..../ 
+............./´¯/'...'/´¯¯`·¸ 
+........../'/.../..../......./¨¯\ 
+........('(...´...´.... ¯~/'...') 
+.........\.................'...../ 
+..........''...\.......... _.·´ 
+............\..............( 
+..............\.............\..."
+  end
+end
+
 get '/' do
-  $message = params[:message] if params[:message]
+  set_message(message) if params[:message]
   $message
 end
 
 put '/' do
-  $message = params[:message]
+  set_message(message) if params[:message]
 end
 
 get '/install' do
