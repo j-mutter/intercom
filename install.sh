2b8e477a237be0574d6e96a3134e136b9603b6cb
Tom Burns <tom.burns@jadedpixel.com>
2013-07-11 13:47:01 -0400

    conditional installer

--- install.sh
+++ install.sh
@@ -11,14 +11,31 @@ function ask {
 
 PREFIX="/usr/local/bin"
 INTERCOM_BIN="$PREFIX/intercom"
-curl -o $INTERCOM_BIN $URL/intercom
 
+NOTIFICATIONS=0
+
+ask "Do you want to install intercom notification messages? [yn]"
+if [ "$?" = "1" ]; then
+  gem install terminal-notifier
+  NOTIFICATIONS=1
+fi
+
+ask "Do you want to install intercom as a service on boot? [yn]"
+SERVICE=$?
+
+ask "Do you want to save all intercoms as an itunes playlist? [yn]"
+PLAYLIST=$?
+
+echo "Installing intercom to $INTERCOM_BIN"
+curl -o $INTERCOM_BIN $URL/intercom
 chmod +x $INTERCOM_BIN
 
-gem install terminal-notifier
+if [ "$SERVICE" = "1" ]; then
+  echo "I am going to ask for SUDO to install intercom as a service. Dont panic!"
+  sudo launchctl remove com.boourns.intercom
+  sudo cp ./com.boourns.intercom.plist /Library/LaunchDaemons/com.boourns.intercom.plist
 
-echo "I am going to ask for SUDO to install intercom as a service. Dont panic!"
-sudo launchctl remove com.boourns.intercom
-sudo cp ./com.boourns.intercom.plist /Library/LaunchDaemons/com.boourns.intercom.plist
-sudo launchctl load -w /Library/LaunchDaemons/com.boourns.intercom.plist
+  sudo launchctl load -w /Library/LaunchDaemons/com.boourns.intercom.plist
+fi
+
 echo "done!"
