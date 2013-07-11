792f261d593acd88d8fe7edf14269c62ec99c28a
Tom Burns <tom.burns@jadedpixel.com>
2013-07-11 13:02:19 -0400

    host installer and script via app

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
 
 PREFIX="/usr/local/bin"
 INTERCOM_BIN="$PREFIX/intercom"
-curl -o $INTERCOM_BIN https://raw.github.com/boourns/intercom/master/intercom.sh
+curl -o $INTERCOM_BIN $INTERCOM_URL/intercom
+
 chmod +x $INTERCOM_BIN
 
 gem install terminal-notifier
 
 echo "I am going to ask for SUDO to install intercom as a service. Dont panic!"
 sudo launchctl remove com.boourns.intercom
-sudo launchctl setenv INTERCOM_REDIS_CONFIG "$INTERCOM_REDIS_CONFIG"
 sudo cp ./com.boourns.intercom.plist /Library/LaunchDaemons/com.boourns.intercom.plist
 sudo launchctl load -w /Library/LaunchDaemons/com.boourns.intercom.plist
 echo "done!"
--- intercom.rb
+++ intercom.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+def access_url
+  ENV['INTERCOM_URL']
+end
+
 get '/' do
   $message
 end
@@ -8,4 +12,13 @@ put '/' do
   $message = params[:message]
 end
 
+get '/install' do
+  installer = File.read('./install.sh')
+  ["#!/usr/bin/env bash", "URL=#{access_url}/", installer].join("\n")
+end
+
+get '/intercom' do
+  script = File.read('./intercom.sh')
+  ["#!/usr/bin/env bash", "URL=#{access_url}/", script].join("\n")
+end
 
